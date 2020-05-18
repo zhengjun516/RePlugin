517e27ff12982ff73ab1f20ef6bdaa5ef11c1dcb
zhengjun <zhengjun516@gmail.com>
2020-05-18 16:22:12 +0800

    [upgrade] update  android-support to androidx

--- replugin-host-library/replugin-host-lib/src/main/java/com/qihoo360/replugin/model/PluginInfoList.java
+++ replugin-host-library/replugin-host-lib/src/main/java/com/qihoo360/replugin/model/PluginInfoList.java
@@ -17,8 +17,9 @@
 package com.qihoo360.replugin.model;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
 
 import com.qihoo360.loader2.Constant;
 import com.qihoo360.replugin.helper.LogDebug;
